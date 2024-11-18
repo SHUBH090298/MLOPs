bee9b1580720049a97577f3db711dcdadefb45dd
SHUBH090298 <shubhneet0902@gmail.com>
2024-11-18 14:58:31 +0100

    update requirements and setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "us_visa",
+    version = "0.0.0",
+    author = "Shubhneet",
+    author_email = "shubhneet0902@gmail.com",
+    packages = find_packages()
+)
